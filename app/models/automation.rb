d3607283c2bca1a58bc9586787e2b29ce93a86c5
Baptiste Archimbaud <baptistarchimbaud@gmail.com>
2026-08-18 15:46:28 +0200

    indentation fixed

--- app/models/automation.rb
+++ app/models/automation.rb
@@ -2,5 +2,4 @@ class Automation < ApplicationRecord
   belongs_to :chat
   validates :llm_provider, uniqueness: { in: %w[OpenAI Anthropic Google] } #  has to be in the list of the 3 providers
   validates :title, presence: true
-
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :chat
-   validates :title, presence: true
+  validates :title, presence: true
 end
