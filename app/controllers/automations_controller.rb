24abc72672c26680095cae9947e1134326bc6614
axel montu <axelmontu@gmail.com>
2026-08-21 16:09:29 +0200

    adding link to automation show

--- app/controllers/automations_controller.rb
+++ app/controllers/automations_controller.rb
@@ -2,4 +2,8 @@ class AutomationsController < ApplicationController
   def index
     @automations = current_user.automations
   end
+
+  def show
+    @automation = Automation.find(params[:id])
+  end
 end
