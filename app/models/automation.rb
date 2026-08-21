1b129b592245ed6e4031a884e2fa0e6ae619f772
Baptiste Archimbaud <baptistarchimbaud@gmail.com>
2026-08-21 11:13:47 +0200

    wip

--- app/models/automation.rb
+++ app/models/automation.rb
@@ -3,4 +3,18 @@ class Automation < ApplicationRecord
   validates :llm_provider, inclusion: { in: %w[OpenAI Anthropic Google] } #  has to be in the list of the 3 providers
   validates :title, presence: true
   validates :description, presence: true
+
+  AUTOMATION_DESCRIPTION = <<~PROMPT
+    Generate a short summarize of the automation based on its content
+  PROMPT
+
+  private
+
+  def generate_description
+    return if content.blank?
+    return if description.present?
+
+    response = RubyLLM.chat.with_instructions(DESCRIPTION_PROMPT).ask(content)
+    update(description: response.content)
+  end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -8,7 +8,7 @@ class Chat < ApplicationRecord
   TITLE_PROMPT = <<~PROMPT
     Generate a short, descriptive, 3-to-6-word title that summarizes the user question for a chat conversation.
   PROMPT
-  def llm 
+  def llm
   # this method takes the parameter we set in the config to easily adapt the thinking effort
     RubyLLM.chat.with_thinking(effort: Rails.application.config.x.llm.thinking_effort)
   end
