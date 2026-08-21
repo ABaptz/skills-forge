d389e7cedb2dbce7ba8d967c37af8feaf65d9f7d
axel montu <axelmontu@gmail.com>
2026-08-21 12:22:25 +0200

    creating turbo stream for automation

--- app/models/automation.rb
+++ app/models/automation.rb
@@ -3,4 +3,11 @@ class Automation < ApplicationRecord
   validates :llm_provider, inclusion: { in: %w[OpenAI openai Anthropic Google] } #  has to be in the list of the 3 providers
   validates :title, presence: true
   validates :description, presence: true
+  after_commit :broadcast_on_chat_show
+
+  private
+
+  def broadcast_on_chat_show
+    broadcast_update_to chat, target: dom_id(self), partial: "chats/automation_content", locals: { automation: self }
+  end
 end
